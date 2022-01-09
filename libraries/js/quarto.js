576c9ca3ee17f144a5921fe34ea224d2bbe7db55
mariniou77 <antonis.marinidis7@gmail.com>
2022-01-09 19:50:43 +0200

    show empty board

---
+++ libraries/js/quarto.js
@@ -0,0 +1,30 @@
+$(function(){
+    draw_empty_board();
+    fill_board();
+});
+
+function draw_empty_board() {
+    var t = '<table id="quarto_table">';
+    for(var i=4; i>0; i--) {
+        t += '<tr>';
+        for(var j = 1; j<5; j++) {
+            t += '<td id="circle_'+i+'_'+j+'">' + '</td>';
+        }
+        t+='</tr>';
+    } 
+    t += '</table>';
+    $('#quarto_board').html(t);
+}
+
+function fill_board(){
+    $.ajax({url: "quarto.php/board/", success: fill_board_by_data}); 
+}
+
+function fill_board_by_data(data) {
+    for(var i=0; i<data.length; i++) {
+        var o = data[i];
+        var id = '#circle_'+ o.posX + '_' + o.posY;
+        var c = (o.piece==null)?o.posX + ',' + o.posY: o.piece;
+        $(id).html(c);
+    }
+}
